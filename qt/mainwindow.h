e9bf7d4e111ea60d2a010cc4969f9ad3902cd02f
tepes-ana-otilia <ana_otilia_tepes@yahoo.com>
2022-04-17 17:08:42 +0300

    Added qt project

---
+++ qt/mainwindow.h
@@ -0,0 +1,54 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_pushButton_inregistrare_clicked();
+
+    void on_pushButton_autentificare_clicked();
+
+    void on_pushButton_inapoi_clicked();
+
+    void on_pushButton_conectare_clicked();
+
+    void on_pushButton_inapoi_2_clicked();
+
+    void on_pushButton_inregistrare_2_clicked();
+
+    void on_pushButton_inapoi_3_clicked();
+
+
+
+    void on_pushButton_prieteni_clicked();
+
+    void on_pushButton_actualizare_date_clicked();
+
+    void on_pushButton_inapoi_5_clicked();
+
+    void on_pushButton_inapoi_4_clicked();
+
+    void on_pushButton_inapoi_6_clicked();
+
+    void on_pushButton_antrenamente_clicked();
+
+    void on_pushButton_inapoi_7_clicked();
+
+    void on_pushButton_vizualizare_evolutie_clicked();
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
